5737056a97e572c8f3acabaf55592fd43670386c
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2026-08-29 12:33:23 +0530

    removed N

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: ["127.0.0.1", "127.0.0.1:19000", "localhost", "localhost:19000"]
+  allowedDevOrigins: ["127.0.0.1", "127.0.0.1:19000", "localhost", "localhost:19000"],
+  devIndicators: false
 };
 
 export default nextConfig;
